0a493641ede3393ccb8171e4f6b02c820f8abda6
Oscar <Oscar@MacBook-Air-de-Marisela.local>
2019-01-27 22:32:47 -0600

    setting up login auth

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,3 +1,36 @@
-const mutations = {};
+const bcrypt = require('bcryptjs'); //hash password, standard technique
+const jwt = require('jsonwebtoken');
+const Mutation = {
+    async createBlog(parent,args,ctx,info){
+        //  TODO: Check if they are logged in
+        const item = await ctx.db.mutation.createBlog({
+            data:{
+                ...args
+            }
+        },info)
+        return item;
+    },
+    async signup(parent, args, ctx, info){
+        args.email = args.email.toLowerCase();
+        const password = await bcrypt.hash(args.password,10);
 
-module.exports = mutations;
+        const user = await ctx.db.mutation.createUser({
+            data: {
+                ...args,
+                password,
+                permissions: { set: ['USER'] },
+            }
+        }, info);
+        //  create the JWT token for them
+        const token = jwt.sign({userId: user.id},process.env.APP_SECRET);
+        ctx.response.cookie('token', token, {
+            httpOnly: true,
+            maxAge: 1000 * 60 * 60 * 24 * 365, //   1 year cookie
+        });
+        return user;
+    }
+
+
+};
+
+module.exports = Mutation;
